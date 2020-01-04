09f296bd40be3b32cb9bcd7d9408221f420eb037
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-04 19:42:26 +0200

    Adding View Folder
    Trying to separate my html code off php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Router Page</title>
+<?php
+ include 'includes/autoloader.inc.php';
+ include 'views/homepage.html';
 
-    </head>
-    <body>
-      Home Page
-    </body>
-</html>
+?>
