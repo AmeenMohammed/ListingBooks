8f17d48f859200f08cf9d0a626af1d20928f1bc5
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-07 15:05:01 +0200

    Start migrating the database

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
- include 'includes/autoloader.inc.php';
+    include 'includes/autoloader.inc.php';
+    include_once './core/stalker_configuration.core.php';
+    include_once './core/stalker_registerar.core.php';
+    include_once './core/stalker_schema.core.php';
+    include_once './core/stalker_validator.core.php';
+    include_once './core/stalker_database.core.php';
+    include_once './core/stalker_information_schema.core.php';
+    include_once './core/stalker_query.core.php';
+    include_once './core/stalker_migrator.core.php';
+    include_once './core/stalker_backup.core.php';
+    include_once './core/stalker_table.core.php';
+    include_once './core/stalker_seed.core.php';
+    include_once './core/stalker_seeder.core.php';
+    include_once './core/stalker_view.core.php';
+
+    foreach ( glob("./tables/*.table.php") as $file ) {
+        require_once $file;
+    }
+    Stalker_Registerar::auto_register();
+    if(Stalker_Migrator::need_migration()){
+        Stalker_Migrator::migrate();
+    }
+  /*  $book = new Books();
+    $book->name = "The Alchemist";
+    $book->quantity = 50;
+    $book->save();
+    $book->name = "The light away";
+    $book->quantity = 50;
+    $book->save();*/
+
     $router = new miniRouter();
     $router->group("/ListingBooks", function($router){
 
