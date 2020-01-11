72e2554603b9e9494c56564abf680d2749462929
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-11 04:18:23 +0200

    Listing books in a dropdown List
    Changing the code for that displays book list in a dropdown
    for much cleaner version. And reformating some of the code to look
    nicer.

--- scripts/request.js
+++ scripts/request.js
@@ -1,102 +1,21 @@
-$(document).ready(function(){
-    var j = 0;
-    var str = new Array();
-    baseURL = window.location.protocol + "//" + window.location.host + "/ListingBooks";
-    $.get(baseURL + "/api/books",{request:1},function(){}, "json").done(function(response){
-        var i;
-        console.log(response);
-        for(i = 0; i < response.length;i++){
-        $.each(response[i], function(key, value) {
-            if(key == 'name'){
-            $('#book0').append($("<option></option>").attr("value",key + i).text(value));
-            }
-       });
-    }
-    loading_selections(response);
+$(document).ready(function () {
+        baseURL = window.location.protocol + "//" + window.location.host + "/ListingBooks";
+        var booksList = [], markup;
 
-    });
-            $('#book0').change(function (){
-                var value = $(this).val();
-                str[0] = value;
-                console.log(value);
-                console.log(str);
-                $("#book1 option").css("display","block");
+        $.get(baseURL + "/api/books", {
+            request: 1
+        }, function () {}, "json").
+        done(function (response) {
+            booksList = response;
+            booksList.forEach(book => {
+                  $('#book0 select.book').append($("<option></option>").attr("value", book.id).text(book.name));
             });
-
-        function first_selction(){
-            $("#book1 option").each(function(){
-                if(($(this).val() == str[0])){
-                       $(this).hide();
-                       console.log("in if!");
-                   }
-              });
-            $('#book0').change(function (){
-                    $("#book1 option").css("display","block");
-                    $("#book1 option").each(function(){
-                        if(($(this).val() == str[0])){
-                               $(this).hide();
-                               console.log("in if!");
-                           }
-                      });
-
-
-            });
-
-        }
-
-    function loading_selections(response){
+            markup = $('#book0').html();
+        });
         $("#add_select").click(function(){
-            if(j < (response.length) - 1){
-                j++;
-                var selectList = document.createElement("select");
-                var input = document.createElement("input");
+            if(booksList.length){
 
-                selectList.setAttribute("class", "select_stlye");
-                selectList.setAttribute("id", "book" + j);
 
-                input.setAttribute("class", "input_stlye");
-                input.setAttribute("id", "input" + j);
-                input.setAttribute("placeholder", "Quantity");
-                $("#selects").append(selectList);
-                $("#selects").append(input);
-                for(i = 0; i < response.length;i++){
-                    if(i == 0){
-                      $('#book' + j).html('<option value=null>Select Book</option>');
-                    }
-                    $.each(response[i], function(key, value) {
-                        if((key == 'name')){
-                        $('#book' + j).append($("<option></option>").attr("value", key + i).text(value));
-                        }
-                   });
-                }
-                        first_selction();
-                        for(var i = 1; i <= j; i++){
-                            $('#book' + i).change(function (){
-                                var value = $(this).val();
-                                str[i] = value;
-                                console.log(value);
-                                $("#book" + i + " option").css("display","block");
-                                $("#book" + i + " option").each(function(){
-                                    for(var h = 0; h < str.length; h++){
-                                        if(($(this).val() == value)||($(this).val() == str[h])){
-                                            $(this).hide();
-                                        }
-                                    }
-                                });
-                        });
-                    }
-                }
-
-            });
-
-            $("#delete_select").click(function(){
-                if(j > 0){
-                    $("#book" + j).remove();
-                    $("#input" + j).remove();
-                    j--;
-                }
-            });
-
-    }
-
+            }
+        });
 });
