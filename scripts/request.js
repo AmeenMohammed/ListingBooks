c3de33c21cc64127dddc2cb1ea5ecc8c8325e976
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-12 19:50:03 +0200

    Mutually exclusive data in muliple drowpdowns

--- scripts/request.js
+++ scripts/request.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
         baseURL = window.location.protocol + "//" + window.location.host + "/ListingBooks";
-        var booksList = [], markup;
+        var booksList = [], markup, j = 1;
+        var selected = [], unselected = [], selectedChange =[];
 
         $.get(baseURL + "/api/books", {
             request: 1
@@ -8,14 +9,56 @@ $(document).ready(function () {
         done(function (response) {
             booksList = response;
             booksList.forEach(book => {
-                  $('#book0 select.book').append($("<option></option>").attr("value", book.id).text(book.name));
+                  $('#book-0 select.book').append($("<option></option>").attr("value", book.id).text(book.name));
             });
-            markup = $('#book0').html();
+            markup = $('#book-0').html();
         });
+
         $("#add_select").click(function(){
-            if(booksList.length){
+            if(booksList.length > j){
+                selected[0] = Number($('select.book').find('option:selected').val());
+                temp = booksList.filter(book =>!selected.includes(book.id));
+                unselected = temp.map(book => book.id);
+                selected.push(unselected.shift());
+               $("#selects").append($("<div></div>").attr("id", "book-" + j).append(markup));
+               $(`#book-${j} option[value=${selected[j]}]`).attr("selected", "selected");
+                $('select.book option').each(function(){
+                    $(this).show();
+                    if(selected.includes(Number(this.value)) && !this.selected){
+                        $(this).hide();
+                    }
+                });
 
-
+                j++;
             }
         });
+        $("#delete_select").click(function(){
+            if(j > 1){
+                $('#book-' + --j).remove();
+                unselected.unshift(selected.pop());
+            }
+            $('select.book option').each(function(){
+                $(this).show();
+                if(selected.includes(Number(this.value)) && !this.selected){
+                    $(this).hide();
+                }
+            });
+        });
+
+        $(document).on( "change", '#selects select.book', function() {
+            for(var i = 0; i < j; i++){
+                selected[i] = (Number($(`#book-${i} select.book`).find('option:selected').val()));
+                temp = booksList.filter(book =>!selectedChange.includes(book.id));
+                unselected = temp.map(book => book.id);
+            }
+                $(`#book-${j} option[value=${selected[j]}]`).attr("selected", "selected");
+                $('select.book option').each(function(){
+                    $(this).show();
+                    if(selected.includes(Number(this.value)) && !this.selected){
+                        $(this).hide();
+                    }
+                });
+
+      });
+
 });
