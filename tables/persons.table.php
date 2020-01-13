97847bffc995446069e3c10113842d33d8fcdc87
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-13 02:11:54 +0200

    Changing name of the table and add date col

--- tables/persons.table.php
+++ tables/persons.table.php
@@ -1,9 +1,10 @@
 <?php
-    class Persons extends Stalker_Table
+    class order extends Stalker_Table
     {
         public function schema() {
             return Stalker_Schema::build( function ($table) {
                 $table->text("name");
+                $table->date("purchasetime")->def(date('Y-m-d H:i:s'));
             });
         }
         public function books() {
